4d4f1ae7fa04d196e8a1b35d43f80a16dd06950c
Ryan Hutchinson <ryanahuthinson@gmail.com>
2017-07-26 20:50:15 -0500

    Initial testing with obd. Working! now it use more json and not while true

---
+++ mainLogger.py
@@ -0,0 +1,60 @@
+import obd
+import arrow
+import json
+import os
+
+
+class Log:
+    def __init__(self):
+        date = arrow.now('US/Central')
+        folder =  "/home/ryan/OBDlogs/" + date.format('MM-DD-YYYY')
+        if not (os.path.isdir(folder)):
+            os.mkdir(folder)
+        filename = date.format('HH_mm_ss')
+        self.toFile = open(folder+filename,'w+')
+        begin = {"id":"BEGIN", "time":arrow.now('US/Central').format('HH:mm:ss')}
+        self.toFile.write(json.dumps(begin))
+
+    def write(self, data):
+	toWrite = {"id": update, "time":arrow.now('US/Central').format('HH:mm:ss'), "Fuel_Level": str(data.fuel_level), "Speed": str(data.speed), "coolant": str(data.coolant)}
+        self.toFile.write(json.dumps(toWrite))
+
+    def close():
+        lastline = {"id":"END", "time":arrow.now('US/Central').format('HH:mm:ss')}
+        self.toFile.write(json.dumps(lastline))
+        self.toFile.close()
+
+class car:
+    def __init__(self):
+        self.connection = obd.OBD()
+	Logger.write(self.connection)
+    def test():
+        cmd = obd.commands["RPM"]
+        response = self.connection.query(cmd)
+	Logger.write(response)
+        return response.value
+
+Logger = Log()
+#tahoe = car()
+#car.test
+#Logger.write(tahoe.test)
+
+connection = obd.OBD()
+
+coolant = obd.commands.COOLANT_TEMP
+speed = obd.commands.SPEED
+fuel_level = obd.commands.FUEL_LEVEL
+throttle_pos = obd.commands.THROTTLE_POS
+data = {}
+
+while True:
+	Logger.write(' Coolant:') 
+	connection.query(coolant).value))
+	data.
+	Logger.write(' speed:')
+	Logger.write(str(connection.query(speed).value))
+	Logger.write(' fuel level:')
+	Logger.write(str(connection.query(fuel_level).value))
+	Logger.write(' Throttle Position')
+	Logger.write(str(connection.query(throttle_pos).value))
+
